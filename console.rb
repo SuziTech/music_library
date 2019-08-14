f33eefa631c9ed3d45643f62ef4409fcc8bff610
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-08-14 14:51:56 +0100

    create initial db and populate artist table

---
+++ console.rb
@@ -0,0 +1,27 @@
+require('pry')
+require_relative('./models/artist')
+require_relative('./models/album')
+
+artist1 = Artist.new( { "name" => "coldplay"} )
+artist2 = Artist.new( { "name" => "the last poets"} )
+
+artist1.save()
+artist2.save()
+
+# album1 = Album.new({
+#   "title" => "greatest hits",
+#   "genre" => "depressing",
+#   "artist_id" => artist1.id
+#   })
+#
+# album2 = Album.new({
+#   "title" => "black summer",
+#   "genre" => "rap",
+#   "artist_id" => artist2.id
+#   })
+
+# album1.save()
+# album2.save()
+
+binding.pry
+nil
